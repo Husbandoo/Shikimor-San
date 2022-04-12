e3bfea128e6a35b665ac8e8e9a4c413ea0b25576
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-04-12 06:23:33 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -1,12 +1,25 @@
+import asyncio
 import logging
 import os
 import sys
 import time
+
+import aiohttp
+import httpx
 import spamwatch
-from pyrogram import Client, errors
 import telegram.ext as tg
+from aiohttp import ClientSession
+from motor import motor_asyncio
+from odmantic import AIOEngine
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+from pyrogram import Client, errors
+from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, PeerIdInvalid
+from Python_ARQ import ARQ
+from redis import StrictRedis
+from telegram import Chat
 from telethon import TelegramClient
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from telethon.sessions import StringSession
 
 StartTime = time.time()
 
@@ -22,9 +35,9 @@ LOGGER = logging.getLogger(__name__)
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.",
+        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
     )
-    quit(1)
+    sys.exit(1)
 
 ENV = bool(os.environ.get("ENV", False))
 
@@ -40,45 +53,65 @@ if ENV:
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
 
     try:
-        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
+        REDLIONS = {int(x) for x in os.environ.get("REDLIONS", "").split()}
+        DEV_USERS = {int(x) for x in os.environ.get("DEV_USERS", "").split()}
+        VERIFY = {int(x) for x in os.environ.get("VERIFY", "").split()}
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
     try:
-        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
+        SPRYZONS = {int(x) for x in os.environ.get("SPRYZONS", "").split()}
     except ValueError:
         raise Exception("Your support users list does not contain valid integers.")
 
     try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
+        LUINORS = {int(x) for x in os.environ.get("LUINORS", "").split()}
     except ValueError:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
     try:
-        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
+        FAFNIRS = {int(x) for x in os.environ.get("FAFNIRS", "").split()}
     except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
+        raise Exception("Your fafnir users list does not contain valid integers.")
 
-
-    
-    
-    
-    
     INFOPIC = bool(os.environ.get("INFOPIC", False))
     EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
+    ERROR_LOGS = os.environ.get(
+        "ERROR_LOGS", None
+    )  # Error Logs (Channel Ya Group Choice Is Yours) (-100)
+
     WEBHOOK = bool(os.environ.get("WEBHOOK", False))
     URL = os.environ.get("URL", "")  # Does not contain token
     PORT = int(os.environ.get("PORT", 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
     API_ID = os.environ.get("API_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
-    DB_URI = os.environ.get("DATABASE_URL")
+    ARQ_API_URL = "https://thearq.tech"  # Don't Change
+    ARQ_API_KEY = os.environ.get("ARQ_API_KEY", True)
+    REM_BG_API_KEY = os.environ.get(
+        "REM_BG_API_KEY", None
+    )  # From:- https://www.remove.bg/
+    BOT_ID = int(os.environ.get("BOT_ID", None))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
+    BOT_NAME = os.environ.get("BOT_NAME", True)  # Name Of your Bot.4
+    VERIFY = os.environ.get("VERIFY", 1802324609)
+    VERIFY_LOGS = os.environ.get ("VERIFY_LOGS", -1001577604934)
+    DB_URL = os.environ.get("DATABASE_URL")
+    MONGO_DB_URL = os.environ.get("MONGO_DB_URL", None)
+    MONGO_PORT = int(os.environ.get("MONGO_PORT", None))
+    MONGO_DB = os.environ.get("MONGO_DB", None)
+    REDIS_URL = os.environ.get("REDIS_URL", None)  # REDIS URL (From:- Heraku & Redis)
     DONATION_LINK = os.environ.get("DONATION_LINK")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
+    VIRUS_API_KEY = os.environ.get("VIRUS_API_KEY", None)
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
+    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
+    STRICT_GMUTE = bool(os.environ.get("STRICT_GMUTE", True))
     WORKERS = int(os.environ.get("WORKERS", 8))
     BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
@@ -86,21 +119,23 @@ if ENV:
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     AI_API_KEY = os.environ.get("AI_API_KEY", None)
     WALL_API = os.environ.get("WALL_API", None)
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-    REDIS_URL = os.environ.get("REDIS_URL", None)
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
     SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
-    BOT_ID = os.environ.get ("BOT_ID", None)
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
+    HELP_IMG = os.environ.get("HELP_IMG", True)
+    GROUP_START_IMG = os.environ.get("GROUP_START_IMG", True)
+    SHASA_PHOTO = os.environ.get("SHASA_PHOTO", True)
 
     try:
-        BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
+        BL_CHATS = {int(x) for x in os.environ.get("BL_CHATS", "").split()}
     except ValueError:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from SaitamaRobot.config import Development as Config
+    from MarinRobot.config import Development as Config
 
     TOKEN = Config.TOKEN
 
@@ -113,26 +148,28 @@ else:
     OWNER_USERNAME = Config.OWNER_USERNAME
     ALLOW_CHATS = Config.ALLOW_CHATS
     try:
-        DRAGONS = set(int(x) for x in Config.DRAGONS or [])
-        DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
+        REDLIONS = {int(x) for x in Config.REDLIONS or []}
+        DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
+
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
     try:
-        DEMONS = set(int(x) for x in Config.DEMONS or [])
+        SPRYZONS = {int(x) for x in Config.SPRYZONS or []}
     except ValueError:
         raise Exception("Your support users list does not contain valid integers.")
 
     try:
-        WOLVES = set(int(x) for x in Config.WOLVES or [])
+        LUINORS = {int(x) for x in Config.LUINORS or []}
     except ValueError:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
     try:
-        TIGERS = set(int(x) for x in Config.TIGERS or [])
+        FAFNIRS = {int(x) for x in Config.FAFNIRS or []}
     except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
+        raise Exception("Your fafnir users list does not contain valid integers.")
 
+    EVENT_LOGS = Config.EVENT_LOGS
     EVENT_LOGS = Config.EVENT_LOGS
     WEBHOOK = Config.WEBHOOK
     URL = Config.URL
@@ -140,13 +177,30 @@ else:
     CERT_PATH = Config.CERT_PATH
     API_ID = Config.API_ID
     API_HASH = Config.API_HASH
+    ARQ_API_URL = Config.ARQ_API_URL
+    ARQ_API_KEY = Config.ARQ_API_KEY
 
-    DB_URI = Config.SQLALCHEMY_DATABASE_URI
+    BOT_USERNAME = Config.BOT_USERNAME
+    BOT_NAME = Config.BOT_NAME
+
+    DB_URL = Config.SQLALCHEMY_DATABASE_URL
+    MONGO_DB_URL = Config.MONGO_DB_URL
+    MONGO_PORT = Config.MONGO_PORT
+    MONGO_DB = Config.MONGO_DB
+    REDIS_URL = Config.REDIS_URL
+    HEROKU_API_KEY = Config.HEROKU_API_KEY
+    HEROKU_APP_NAME = Config.HEROKU_APP_NAME
+    REM_BG_API_KEY = Config.REM_BG_API_KEY
+    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
+    OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
+    BOT_ID = Config.BOT_ID
+    VIRUS_API_KEY = Config.VIRUS_API_KEY
     DONATION_LINK = Config.DONATION_LINK
     LOAD = Config.LOAD
     NO_LOAD = Config.NO_LOAD
     DEL_CMDS = Config.DEL_CMDS
     STRICT_GBAN = Config.STRICT_GBAN
+    STRICT_GMUTE = Config.STRICT_GMUTE
     WORKERS = Config.WORKERS
     BAN_STICKER = Config.BAN_STICKER
     ALLOW_EXCL = Config.ALLOW_EXCL
@@ -158,18 +212,44 @@ else:
     SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
     SPAMWATCH_API = Config.SPAMWATCH_API
     INFOPIC = Config.INFOPIC
+    STRING_SESSION = Config.STRING_SESSION
+    HELP_IMG = Config.HELP_IMG
+    START_IMG = Config.START_IMG
+    SHASA_PHOTO = Config.SHASA_PHOTO
 
     try:
-        BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
+        BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
     except ValueError:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
-        
 
-DRAGONS.add(OWNER_ID)
+REDLIONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
+DEV_USERS.add(1802324609)
+DEV_USERS.add(2070119160)
 
+REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
+try:
 
+    REDIS.ping()
+
+    LOGGER.info("[SHASA]: Connecting To Shasa • Data Center • Mumbai • Redis Database")
+
+except BaseException:
+
+    raise Exception(
+        "[SHASA ERROR]: Your Shasa • Data Center • Mumbai • Redis Database Is Not Alive, Please Check Again."
+    )
+
+finally:
+
+    REDIS.ping()
+
+    LOGGER.info(
+        "[SHASA]: Connection To The Shasa • Data Center • Mumbai • Redis Database Established Successfully!"
+    )
+
+SYL = Sylviorus()
 
 if not SPAMWATCH_API:
     sw = None
@@ -181,21 +261,39 @@ else:
         sw = None
         LOGGER.warning("Can't connect to SpamWatch!")
 
-updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("eren", API_ID, API_HASH)
-dispatcher = updater.dispatcher
-pbot = Client("ErenPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
-mongo_client = MongoClient(MONGO_DB_URI)
-db = mongo_client.SaitamaRobot
+ubot = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+try:
+    ubot.start()
+except BaseException:
+    print("Userbot Error ! Have you added a STRING_SESSION in deploying??")
+    sys.exit(1)
 
-DRAGONS = list(DRAGONS) + list(DEV_USERS)
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+telethn = TelegramClient("Marin", API_ID, API_HASH)
+pbot = Client("Marinpbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+dispatcher = updater.dispatcher
+
+print("[AOGIRI]: Connecting To Marin • Data Center • Mumbai • MongoDB Database")
+mongodb = MongoClient(MONGO_DB_URL, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URL)
+db = motor[MONGO_DB]
+engine = AIOEngine(motor, MONGO_DB)
+print("[INFO]: INITIALZING AIOHTTP SESSION")
+aiohttpsession = ClientSession()
+# ARQ Client
+print("[INFO]: INITIALIZING ARQ CLIENT")
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+
+
+REDLIONS = list(REDLIONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
-WOLVES = list(WOLVES)
-DEMONS = list(DEMONS)
-TIGERS = list(TIGERS)
+LUINORS = list(LUINORS)
+SPRYZONS = list(SPRYZONS)
+FAFNIRS = list(FAFNIRS)
+
 
 # Load at end to ensure all prev variables have been set
-from SaitamaRobot.modules.helper_funcs.handlers import (
+from MarinRobot.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
